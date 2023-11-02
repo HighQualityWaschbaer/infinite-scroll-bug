cd248c1a42b11d2744c91f0af8a4ce8a3075481a
HighQualityWaschbaer <simonrach@outlook.com>
2023-11-02 09:01:24 +0100

    [ADD] Added example.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  nextNum = 0;
+  numbers: Number[] = [];
 
   constructor() {}
 
+  ngOnInit(): void {
+    this.addNumbers();
+  }
+
+  addNumbers() {
+    for(let i = 0; i < 10; i++) {
+      this.numbers.push(this.nextNum);
+      this.nextNum++;
+    }
+  }
+
+  onInfiniteScroll(e: any) {
+    console.log("Infinite scroll fired");
+    this.addNumbers();
+    e.target.complete();
+  }
 }
