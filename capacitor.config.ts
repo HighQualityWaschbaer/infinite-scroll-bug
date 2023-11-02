dce9c81575b7d7e8c826aa8b58bf9431b1f1f63f
HighQualityWaschbaer <simonrach@outlook.com>
2023-11-02 08:42:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'infinite-scroll-bug',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
